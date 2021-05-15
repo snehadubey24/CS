3a95633869357b8daa76ad351ddf4982f45b27be
Aman <aakashaman1770@gmail.com>
2021-05-15 17:00:02 +0530

    intial commit

---
+++ CLRS/CO2 Getting-Started/Chapter_Brief/insertion_sort.cpp
@@ -0,0 +1,62 @@
+// PseudoCode
+
+//        INSERTION-SORT(A)
+
+//        for j = 2 to A.length
+//            key = A[j]
+//            //Insert A[j] into the sorted sequence A[1...j-1]
+//            i = j -1
+//            while i > 0 and A[i] > key
+//                A[i+1] = A[i]
+//                i = i - 1
+//            A[i + 1] = key
+
+//        Time Conplexity : O(n^2)
+
+//Driver Program
+
+#include <iostream>
+using namespace std;
+
+void insertionSort(int A[], int size)
+{
+
+    for (int j = 2; j < size; j++)
+    {
+        int key = A[j];
+
+        //Insert A[j] into the sorted sequence A[1...j-1]
+        int i = j - 1;
+
+        while (i > 0 && A[i] > key)
+        {
+            A[i + 1] = A[i];
+            i--;
+        }
+
+        A[i + 1] = key;
+    }
+}
+
+void printArr(int A[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << A[i] << " ";
+    }
+}
+
+int main()
+{
+    //Array Initialization
+    int A[] = {31, 41, 59, 26, 41, 58};
+
+    int size = sizeof(A[0]) / sizeof(A);
+
+    //Sorting A
+    insertionSort(A, size);
+
+    printArr(A, size);
+
+    return 0;
+}
