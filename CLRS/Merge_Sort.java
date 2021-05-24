25e88c0e5bfe9f512e47ec2214b2059256c6fa09
Aman <aakashaman1770@gmail.com>
2021-05-24 12:38:10 +0530

    Initial commit

--- CLRS/Merge_Sort.java
+++ CLRS/Merge_Sort.java
@@ -1,6 +1,14 @@
-import java.util.io;
 
-public static void main(String[] args){
-    
+public class Merge_Sort {
+    public static void MERGE(int arr[], int low, int mid, int high) {
+        int n1 = mid - low + 1;
+        int n2 = high - mid;
+
+        int L[] = new int[n1 + 1];
+    }
+
+    public static void main(String[] args) {
+
+    }
 
 }
